79c35c0e8df80be47131c1073fce8f3775711557
Craig Booker <info@arcadiasoftworks.com>
2019-01-24 14:43:40 -0600

    Initial Commit

---
+++ Views/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Views
+//
+//  Created by Craig Booker on 1/24/19.
+//  Copyright © 2019 Arcadia Softworks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ Views/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Views
+//
+//  Created by Craig Booker on 1/24/19.
+//  Copyright © 2019 Arcadia Softworks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
